3afd931ace7c6021854d4dbd1a52e0e8d039c645
John Vandenberg <jayvdb@gmail.com>
2024-09-10 04:59:09 +0800

    Use proc-macro-error2

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! simpler API.
 //!
 //! ```rust
-//! use sea-bae::FromAttributes;
+//! use sea_bae::FromAttributes;
 //!
 //! #[derive(
 //!     Debug,
@@ -105,7 +105,7 @@ extern crate proc_macro;
 
 use heck::ToSnakeCase;
 use proc_macro2::TokenStream;
-use proc_macro_error::*;
+use proc_macro_error2::{abort, proc_macro_error};
 use quote::*;
 use syn::{spanned::Spanned, *};
 
